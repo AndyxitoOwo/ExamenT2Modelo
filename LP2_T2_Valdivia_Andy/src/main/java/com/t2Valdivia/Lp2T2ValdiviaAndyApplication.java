2c4defd65008e4e81ecad9325cb9fd16eef396a2
AndyxitoOwo <I202407449@cibertec.edu.pe>
2025-06-16 20:35:35 -0500

    Proyecto Iniciado

---
+++ LP2_T2_Valdivia_Andy/src/main/java/com/t2Valdivia/Lp2T2ValdiviaAndyApplication.java
@@ -0,0 +1,13 @@
+package com.t2Valdivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2ValdiviaAndyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2ValdiviaAndyApplication.class, args);
+	}
+
+}
---
+++ LP2_T2_Valdivia_Andy/src/test/java/com/t2Valdivia/Lp2T2ValdiviaAndyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2Valdivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2ValdiviaAndyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
